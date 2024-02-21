9621c725d344fd303cc35aab5bed9219cd50088e
joselyncalderonrios <joselyncalderon@gmail.com>
2024-02-21 17:58:33 -0500

    primer-commit reto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PersonaComponent } from './persona/persona/persona.component';
+import { HttpClientModule } from '@angular/common/http'; // Cambio aquí
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule ,
+    MatTableModule, 
+    MatPaginatorModule,
+   
+    BrowserAnimationsModule, // Importa BrowserAnimationsModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
